87e37c861bf4d3737ab007c59e24af708c24d974
高山雅明 <masaaki19951226@gmail.com>
2025-02-24 16:06:14 +0900

    a

--- src/pages/post.js
+++ src/pages/post.js
@@ -3,10 +3,23 @@ import { graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
-import single from "/src/css/single.css"
+import HTMLReactParser from 'html-react-parser'; // html-to-reactライブラリをインポート
+import single from "/src/css/single.css";
 
 const Post = ({ data }) => {
   const post = data.wpPost;
+
+  // HTMLReactParserでコンテンツから<p>タグを抽出
+  const parsedContent = HTMLReactParser(post.content);
+  const firstParagraph = parsedContent.find(
+    (element) => typeof element === 'object' && element.type === 'p'
+  );
+  let truncatedText = '';
+
+  if (firstParagraph && typeof firstParagraph.props.children === 'string') {
+    const text = firstParagraph.props.children;
+    truncatedText = text.length > 50 ? text.slice(0, 50) + '...' : text;
+  }
 
   return (
     <Layout>
@@ -15,7 +28,14 @@ const Post = ({ data }) => {
         <div className="container">
           <h2>作品詳細</h2>
           <h3>{post.title}</h3>
+          {post.featuredImage?.node?.localFile?.childImageSharp?.gatsbyImageData && (
+            <GatsbyImage
+              image={post.featuredImage.node.localFile.childImageSharp.gatsbyImageData}
+              alt={post.title}
+            />
+          )}
           <div dangerouslySetInnerHTML={{ __html: post.content }} />
+          <p className="category-item__text">{truncatedText}</p>
         </div>
       </section>
     </Layout>
